0cde164d64464408dd48bc0801fb4b8112e8db59
Sarah Appleyard <sarah.m.appleyard-1@ou.edu>
2018-11-10 15:07:40 -0600

    Project 1 Sarah Appleyard

---
+++ Desktop/CS Projects/src/Project1_SarahAppleyard.java
@@ -0,0 +1,16 @@
+/** This program prints out fun facts about CS. 
+ * @author Sarah M. Appleyard
+ * @version 1.0
+ * I copied this code from the Project 1 Handout, but it's OK because the other Sarah told me to.
+ */
+public class Project1_SarahAppleyard 
+{
+	//This is the main program. The instructions here
+	//are what is done when the program is run
+	public static void main(String[] args)
+	{
+		//Again, replace my name with yours on the line below. Don't change
+		//any punctuation.
+		System.out.println("Hello World, my name is: Sarah Appleyard");
+	}
+}
